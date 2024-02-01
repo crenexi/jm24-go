64965bcbc0601635522a090ff93b0043e8ff5d5f
James Blume <james@crenexi.com>
2024-02-01 12:36:41 -0800

    Cleanup

--- config/metadata.js
+++ config/metadata.js
@@ -1,9 +1,9 @@
 // prettier-ignore
 const metadata = {
   robots: 'all',
-  title: 'Yoda',
-  themeColor: '#ffffff',
-  hrefGoogleFonts: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&family=PT+Sans+Narrow&display=swap',
+  title: 'J&M Wedding',
+  themeColor: '#705d7e',
+  hrefGoogleFonts: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&family=Handlee&display=swap',
 };
 
 module.exports = metadata;
