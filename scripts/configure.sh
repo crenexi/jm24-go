777a6d62a99a5d094dd5007a28723c04e514dfbf
James Blume <james@crenexi.com>
2024-03-14 11:53:30 -0700

    Test deployment fix

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-# Load .env file
-if [ ! -f .env ]; then
-  echo ".env file not found"
-  exit 1
-fi
-source .env
+# Function to check if CodeArtifact variables are set
+verify_env_vars() {
+  if [ -z "$CODEARTIFACT_REGION" ] || [ -z "$CODEARTIFACT_OWNER" ] || [ -z "$CODEARTIFACT_DOMAIN" ] || [ -z "$CODEARTIFACT_REPO" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
 
-# Ensure all necessary CodeArtifact variables are set
-if [ -z "$CODEARTIFACT_REGION" ] || [ -z "$CODEARTIFACT_OWNER" ] || [ -z "$CODEARTIFACT_DOMAIN" ] || [ -z "$CODEARTIFACT_REPO" ]; then
-  echo "Error: One or more CodeArtifact variables are not set."
-  exit 1
+# Try loading .env file if required env vars are not already set
+if ! verify_env_vars; then
+  if [ -f .env ]; then
+    echo "Sourcing environment variables from .env file..."
+    source .env
+
+    # Re-check if the necessary variables are set after sourcing .env
+    if ! verify_env_vars; then
+      echo "Error: One or more CodeArtifact variables are not set."
+      exit 1
+    fi
+  else
+    echo ".env file not found and environment variables not set."
+    exit 1
+  fi
+else
+  echo "CodeArtifact environment variables already set."
 fi
 
 # Set local variables for readability
@@ -23,5 +38,5 @@ repo=$CODEARTIFACT_REPO
 npm config delete registry
 
 # Connect with package repos
-echo "Connecting with crenexi CodeArtifact repository..."
+echo "Connecting with CodeArtifact repository..."
 aws --region $region codeartifact login --tool npm --domain-owner $owner --domain $domain --repository $repo
